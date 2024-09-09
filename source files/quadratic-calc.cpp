10779f4f3cf23b66cee1c6f56b3f402d80037ea2
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-09 15:59:46 -0400

    feat: Starting quadratic-calc.cpp for calculating roots of a quadratic equation

---
+++ source files/quadratic-calc.cpp
@@ -0,0 +1,7 @@
+//The purpose of this assignment is to develop a C++ program that:
+//calculates the roots of a quadratic equation using the quadratic formula. 
+//This will help students practice working with functions, conditional statements, input/output operations, and mathematical computations.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
