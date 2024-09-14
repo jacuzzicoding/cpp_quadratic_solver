78f846753c2416c22ac15e2fd41ce75f439bc06b
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-14 19:17:16 -0400

    feat: Add prime_finder.cpp for finding prime numbers and divisors

---
+++ source files/prime_finder.cpp
@@ -0,0 +1,23 @@
+//Brock J Prime Number Program
+//It will tell you if a given number is prime or not, and provide a list of all the divisors of that number.
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+// Function prototypes
+bool isPrime(int n);
+void printDivisors(int n);
+
+// main program loop
+int main() {
+    double n;
+    char choice;
+
+//doing a do loop here so it can be used multiple times without restart
+    do {
+        cout << "This program will find all the prime numbers  between 1 and the number you input.\n";
+        cout << "Enter a number: ";
+    }
+}
