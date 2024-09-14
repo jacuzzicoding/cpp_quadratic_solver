4f6bb65c91277381a6f06e2a4e5fd2543a3f62a6
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-14 19:45:01 -0400

    feat: Refactor prime_finder.cpp for improved efficiency and readability

--- source files/prime_finder.cpp
+++ source files/prime_finder.cpp
@@ -15,9 +15,10 @@ int main() {
     double n;
     char choice;
 
-//doing a do loop here so it can be used multiple times without restart
+//the intro message will be outside of the do loop so it only prints once. 
+ cout << "This program will tell you if a number is prime or not, and provide a list of all the divisors of that number.\n";
+   //implementing a do loop here so it can be used multiple times without restart
     do {
-        cout << "This program will tell you if a number is prime or not, and provide a list of all the divisors of that number.\n";
         cout << "Enter a number: ";
         cin >> n;
 //error check to make sure the number is positive
@@ -44,11 +45,28 @@ int main() {
 
 //now for the functions
 //isPrime function will check if the number is prime
-bool isPrime(int n);
+bool isPrime(int n)
     {
         if (n <= 1) {
             return false;
         }
-        for (int )
+//if the number is 2 or greater, we will check if it has any divisors. If it does, it is not prime and it will return false
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+//if it has no divisors, it is prime and will return true
+        return true;
     }
-    ]
+//now we will define the printDivisors function to print all the divisors of the number! 
+void printDivisors(int n) {
+    cout << "Thank you!\nThe divisors of " << n << " are: ";
+    //loop through all the numbers from 1 to n and check if they are divisors of n. If they are, it will print them
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
