a9fe80ba04f8e0d825e03de270c970f11c166c75
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-15 19:38:17 -0400

    feat: Refactor quad_calculator.cpp for improved readability and documentation

--- source files/quad_calculator.cpp
+++ source files/quad_calculator.cpp
@@ -10,21 +10,19 @@
 using namespace std;
 
 // Function prototypes because I put the function definitions after the main program loop 
-void quadraticRoots(int a, int b, int c);
-int discriminant(int a, int b, int c);
-void calculateRoots(int a, int b, int c, int disc);
+void quadraticRoots(double a, double b, double c);
+double discriminant(double a, double b, double c);
+void calculateRoots(double a, double b, double c, double disc);
 
 // main program loop begins here
 int main() {
-    int a, b, c;
+    double a, b, c;
     char choice;
-
 //intro message before do while loop
     cout << "This program calculates the roots of a quadratic equation in the form of ax^2 + bx + c.\n";
 //here we will use a do while loop to allow the user to solve multiple equations without restarting the program. 
-//Unfortunately, I cannot get the program to loop back to the beginning after the user has solved an equation. I'm not sure why, because it works on the prime_finder.cpp, but I will keep working on it.
     do {
-        cout << "Enter the coefficients of the quadratic equation (a, b, and c): ";
+        cout << "Enter the coefficients of the quadratic equation (a b c) seperated by spaces: ";
         cin >> a >> b >> c;
 //error checking to make sure the equation is quadratic. if a is 0, it is not a quadratic equation.
         if (a == 0) {
@@ -40,30 +38,40 @@ int main() {
 }
 
 // implementing the function prototypes. discriminant function is the b^2 - 4ac part of the quadratic formula
-int discriminant(int a, int b, int c) {
+double discriminant(double a, double b, double c) {
     return (b*b) - (4 * a * c);
 }
 // calculateRoots function will calculate the roots, or the "x's" of the quadratic equation
-void calculateRoots(int a, int b, int c, int disc) {
+
+void calculateRoots(double a, double b, double c, double disc) {
+    // set the precision of the output to 2 decimal places
     cout << fixed << setprecision(2);
-    if (disc >= 0) {
-//using the quadratic formula to calculate the roots.
-        int root1 = (-b + sqrt(disc)) / (2 * a);
-        int root2 = (-b - sqrt(disc)) / (2 * a);
-        cout << "The roots of the quadratic equation are: " << root1 << " and " << root2 << endl;
-    } else {
-//if the disc is negative, we have complex roots. so we will calculate the real part and the imaginary part
-        int realPart = -b / (2 * a);
-        int imagPart = sqrt(-disc) / (2 * a);
+    // check if the discriminant is positive so we can calculate the roots
+    if (disc > 0) {
+        double root1 = (-b + sqrt(disc)) / (2 * a);
+        double root2 = (-b - sqrt(disc)) / (2 * a);
+        cout << "The roots of the equation are: " << root1 << " and " << root2 << endl;
+    } 
+    // if the discriminant is 0, we have one real root
+    else if (disc == 0) {
+        double root = -b / (2 * a);
+        cout << "The root of the equation is: " << root << endl;
+    } 
+    // if the discriminant is negative, we have two complex roots
+    else {
+        double realPart = -b / (2 * a);
+        double imagPart = sqrt(-disc) / (2 * a);
         cout << "The roots of the equation are: " 
              << realPart << " + " << imagPart << "i and "
              << realPart << " - " << imagPart << "i" << endl;
     }
 }
 
+
+
 // the quadraticRoots function
-void quadraticRoots(int a, int b, int c) {
-    int disc = discriminant(a, b, c);
+void quadraticRoots(double a, double b, double c) {
+    double disc = discriminant(a, b, c);
 //check if the disc is positive, if it is, we know we have two real roots and will tell the user
     if (disc > 0) {
         cout << "The quadratic equation has two distinct real roots." << endl;
